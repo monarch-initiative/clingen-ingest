cf88bdb40e9a6eb92dcb4b83a8bd586375ee5aad
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2024-06-25 16:57:27 -0500

    Update correct row test

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -26,26 +26,26 @@ def map_cache():
 @pytest.fixture
 def correct_row():
     return {
-        'Variation': 'NM_000277.2(PAH):c.581T>C (p.Leu194Pro)',
-        'ClinVar Variation Id': '102742',
-        'Allele Registry Id': 'CA229633',
-        'HGVS Expressions': 'NM_000277.2:c.581T>C',
+        'Variation': 'NM_000277.2(PAH):c.1A>G (p.Met1Val)',
+        'ClinVar Variation Id': '586',
+        'Allele Registry Id': 'CA114360',
+        'HGVS Expressions': 'NM_000277.2:c.1A>G, NC_000012.12:g.102917130T>C, CM000674.2:g.102917130T>C',
         'HGNC Gene Symbol': 'PAH',
         'Disease': 'phenylketonuria',
         'Mondo Id': 'MONDO:0009861',
         'Mode of Inheritance': 'Autosomal recessive inheritance',
-        'Assertion': 'Likely Pathogenic',
-        'Applied Evidence Codes (Met)': 'PP3, PP4_Moderate, PM2, PM3_Strong',
-        'Applied Evidence Codes (Not Met)': '',
-        'Summary of interpretation': 'PAH-specific ACMG/AMP criteria applied: PM2: Absent from 1000G',
-        'PubMed Articles': '9012412, 16601866, 8533759, 7981714, 16601866, 7981714',
+        'Assertion': 'Pathogenic',
+        'Applied Evidence Codes (Met)': 'PS3, PP4_Moderate, PM2, PM3',
+        'Applied Evidence Codes (Not Met)': 'PVS1',
+        'Summary of interpretation': 'PAH-specific ACMG/AMP criteria applied: PM2: gnomAD MAF=0.00002',
+        'PubMed Articles': '9450897, 2574002, 2574002',
         'Expert Panel': 'Phenylketonuria VCEP',
-        'Guideline': 'https://clinicalgenome.org/docs/clingen-pah-expert-panel-specifications-to-the-tuncated.../',
-        'Approval Date': '2018-08-13',
-        'Published Date': '2019-04-06',
+        'Guideline': 'https://clinicalgenome.org/docs/clingen-pah-expert-panel-specifications-to-TRUNCATED/',
+        'Approval Date': '2019-03-23',
+        'Published Date': '2019-05-10',
         'Retracted': 'false',
-        'Evidence Repo Link': 'https://erepo.genome.network/evrepo/ui/classification/CA229633/MONDO:0009861/006',
-        'Uuid': '434f1539-4967-4ff7-abc2-e2ff3ca9ecbe',
+        'Evidence Repo Link': 'https://erepo.genome.network/evrepo/ui/classification/CA114360/MONDO:0009861/006',
+        'Uuid': '89f04437-ed5d-4735-8c4a-a9b1d91d10ea'
     }
 
 
@@ -87,17 +87,17 @@ def test_correct_row(correct_entities):
     assert len(correct_entities) == 2
     entity_a, entity_b = correct_entities
     # test entity_a
-    assert entity_a.id == 'CLINVAR:102742'
-    assert entity_a.name == 'NM_000277.2(PAH):c.581T>C (p.Leu194Pro)'
-    assert entity_a.xref == ['CA229633']
+    assert entity_a.id == 'CLINVAR:586'
+    assert entity_a.name == 'NM_000277.2(PAH):c.1A>G (p.Met1Val)'
+    assert entity_a.xref == ['CA114360']
     assert entity_a.in_taxon == ['NCBITaxon:9606']
     assert entity_a.in_taxon_label == 'Homo sapiens'
 
     # test entity_b
-    assert entity_b.subject == 'CLINVAR:102742'
+    assert entity_b.subject == 'CLINVAR:586'
     assert entity_b.predicate == 'biolink:causes'
     assert entity_b.negated is False
-    assert entity_b.original_predicate == 'Likely Pathogenic'
+    assert entity_b.original_predicate == 'Pathogenic'
     assert entity_b.object == 'MONDO:0009861'
     assert entity_b.primary_knowledge_source == 'infores:clingen'
     assert entity_b.aggregator_knowledge_source == ['infores:monarchinitiative']
