357c4b0fb31e823dcd03852ddcf4c416e4497376
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2024-06-26 16:48:00 -0500

    Finish TODO's and testing

--- src/clingen_ingest/transform.py
+++ src/clingen_ingest/transform.py
@@ -15,23 +15,24 @@ hgnc_gene_lookup = koza_app.get_map('hgnc_gene_lookup')
 CAUSES = "biolink:causes"
 IS_SEQUENCE_VARIANT_OF = 'biolink:is_sequence_variant_of'
 CONTRIBUTES_TO = "biolink:contributes_to"
-RELATED_TO = "biolink:related_to"
-
-seen_variants = {}
+ASSOCIATED_WITH = "biolink:associated_with"
 
 
-def get_disese_predicate_and_negation(clinical_significance):
+def get_disease_predicate_and_negation(clinical_significance):
     if clinical_significance == 'Benign' or clinical_significance == 'Likely Benign':
         return CONTRIBUTES_TO, True
     elif clinical_significance == 'Likely Pathogenic' or clinical_significance == 'Pathogenic':
         return CAUSES, False
     elif clinical_significance == 'Uncertain Significance':
-        # TODO: not sure how we to represent uncertain significance
-        return RELATED_TO, False
+        # DONE: not sure how we should represent uncertain significance
+        # Based on my reading of the Biolink Model and a sampling of these rows
+        # I think "biolink:associated_with" is the most appropriate predicate
+        return ASSOCIATED_WITH, False
     else:
         raise ValueError(f"Not sure how to handle _assertion: '{clinical_significance}'")
 
 
+seen_variants = {}
 while (row := koza_app.get_row()) is not None:
     # Code to transform each row of data
     # For more information, see https://koza.monarchinitiative.org/Ingests/transform
@@ -40,28 +41,38 @@ while (row := koza_app.get_row()) is not None:
     if row["Retracted"] == "true":
         continue
 
-    # Initially, skip rows with no variant, TODO: Revisit this decision after consulting in the general context of g2d
-    if row["ClinVar Variation Id"] == "-" or row["Variation"] == "":
-        continue
+    # DONE: Initially skipping rows with no variant; revisit this decision in the general context of g2d
+    # These look like meaningful variants, so I'm transforming them
+    # When there is no 'ClinVar Variation Id', use 'Allele Registry Id' as the variant_id
+    if row["ClinVar Variation Id"] == "-":
+        variant_id = row['Allele Registry Id']
+    else:
+        variant_id = "CLINVAR:{}".format(row['ClinVar Variation Id'])
 
-    variant_id = "CLINVAR:{}".format(row['ClinVar Variation Id'])
+    # When there is no 'Variation', use the first entry in 'HGVS Expressions' as the variant_name
+    if row["Variation"] == "":
+        variant_name = row['HGVS Expressions'].split(",")[0]
+    else:
+        variant_name = row["Variation"]
 
     gene_symbol = row['HGNC Gene Symbol']
-    gene_id = "HGNC:" + hgnc_gene_lookup.get(gene_symbol)['hgnc_id'] if gene_symbol in hgnc_gene_lookup else None
+    gene_id = hgnc_gene_lookup.get(gene_symbol)['hgnc_id'] if gene_symbol in hgnc_gene_lookup else None
     original_disease_predicate = row["Assertion"]
     if variant_id not in seen_variants:
+        seen_variants[variant_id] = variant_id
         entities.append(
             SequenceVariant(
                 id=variant_id,
-                name=row['Variation'],
+                name=variant_name,
                 xref=[row['Allele Registry Id']],
-                # There are no duplicate variants, so we can just populate has_gene and not worry about multiples
-                has_gene= [gene_id] if gene_id is not None else None,
+                # DONE: populate has_gene the first time, and then append for multiple genes?
+                # There is only one duplicate variant, and they appear to be identical, so I'm skipping this for now
+                has_gene=[gene_id] if gene_id is not None else None,
                 in_taxon=['NCBITaxon:9606'],
                 in_taxon_label='Homo sapiens',
             )
         )
-    predicate, negated = get_disese_predicate_and_negation(original_disease_predicate)
+    predicate, negated = get_disease_predicate_and_negation(original_disease_predicate)
     entities.append(
         VariantToDiseaseAssociation(
             id=str(uuid.uuid4()),
@@ -81,7 +92,9 @@ while (row := koza_app.get_row()) is not None:
             VariantToGeneAssociation(
                 id=str(uuid.uuid4()),
                 subject=variant_id,
-                predicate=IS_SEQUENCE_VARIANT_OF,  # TODO: more specific predicates? is_missense_variant_of etc
+                # DONE: more specific predicates? is_missense_variant_of etc.
+                # I don't see any fields that state type of variant, so I don't think we can be more specific
+                predicate=IS_SEQUENCE_VARIANT_OF,
                 object=gene_id,
                 primary_knowledge_source="infores:clingen",
                 aggregator_knowledge_source=["infores:monarchinitiative"],
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -21,7 +21,7 @@ TRANSFORM_SCRIPT = "./src/clingen_ingest/transform.py"
 def map_cache():
     # Add gene lookup values to map_cache for HGNC lookup.
     # NOTE: This must match the HGNC information for 'HGNC Gene Symbol' in correct row
-    return {"hgnc_gene_lookup": {"PAH": {"hgnc_id": "8582"}}}
+    return {"hgnc_gene_lookup": {"PAH": {"hgnc_id": "HGNC:8582"}}}
 
 
 # Define an example row to test (as a dictionary)
@@ -120,21 +120,98 @@ def test_retracted_row(retracted_entities):
     assert len(retracted_entities) == 0
 
 
+# Define the mock koza transform for a row with an empty variation
 @pytest.fixture
 def empty_variation(mock_koza, correct_row, map_cache):
     correct_row["Variation"] = ""
     return mock_koza(INGEST_NAME, correct_row, TRANSFORM_SCRIPT, map_cache=map_cache)
 
 
+# Test the output of the transform for a row with an empty variation
 def test_empty_variation(empty_variation):
-    assert len(empty_variation) == 0
+    assert len(empty_variation) == 3
+    assert empty_variation[0].name == "NM_000277.2:c.1A>G"
 
 
+# Define the mock koza transform for a row with a missing entity_id
 @pytest.fixture
 def missing_entity_id(mock_koza, correct_row, map_cache):
     correct_row["ClinVar Variation Id"] = "-"
     return mock_koza(INGEST_NAME, correct_row, TRANSFORM_SCRIPT, map_cache=map_cache)
 
 
+# Test the output of the transform for a row with a missing entity_id
 def test_missing_entity_id(missing_entity_id):
-    assert len(missing_entity_id) == 0
+    assert len(missing_entity_id) == 3
+    assert missing_entity_id[0].id == "CA114360"
+
+
+# Define the mock koza transform for a row with 'Benign' as the clinical_significance
+@pytest.fixture
+def correct_entities_benign(mock_koza, correct_row, map_cache):
+    correct_row["Assertion"] = "Benign"
+    return mock_koza(INGEST_NAME, correct_row, TRANSFORM_SCRIPT, map_cache=map_cache)
+
+
+# Test 'clinical_significance' values of 'Benign'
+def test_correct_row_benign(correct_entities_benign):
+    assert len(correct_entities_benign) == 3
+    entity, association_a, association_b = correct_entities_benign
+    assert association_a.predicate == 'biolink:contributes_to'
+    assert association_a.negated is True
+    assert association_a.original_predicate == 'Benign'
+
+
+# Define the mock koza transform for a row with 'Likely Benign' as the clinical_significance
+@pytest.fixture
+def correct_entities_likely_benign(mock_koza, correct_row, map_cache):
+    correct_row["Assertion"] = "Likely Benign"
+    return mock_koza(INGEST_NAME, correct_row, TRANSFORM_SCRIPT, map_cache=map_cache)
+
+
+# Test 'clinical_significance' values of 'Likely Benign'
+def test_correct_row_likely_benign(correct_entities_likely_benign):
+    assert len(correct_entities_likely_benign) == 3
+    entity, association_a, association_b = correct_entities_likely_benign
+    assert association_a.predicate == 'biolink:contributes_to'
+    assert association_a.negated is True
+    assert association_a.original_predicate == 'Likely Benign'
+
+
+# Define the mock koza transform for a row with 'Likely Pathogenic' as the clinical_significance
+@pytest.fixture
+def correct_entities_likely_pathogenic(mock_koza, correct_row, map_cache):
+    correct_row["Assertion"] = "Likely Pathogenic"
+    return mock_koza(INGEST_NAME, correct_row, TRANSFORM_SCRIPT, map_cache=map_cache)
+
+
+# Test 'clinical_significance' values of 'Likely Pathogenic'
+def test_correct_row_likely_pathogenic(correct_entities_likely_pathogenic):
+    assert len(correct_entities_likely_pathogenic) == 3
+    entity, association_a, association_b = correct_entities_likely_pathogenic
+    assert association_a.predicate == 'biolink:causes'
+    assert association_a.negated is False
+    assert association_a.original_predicate == 'Likely Pathogenic'
+
+
+# Define the mock koza transform for a row with 'Uncertain Significance' as the clinical_significance
+@pytest.fixture
+def correct_entities_uncertain_significance(mock_koza, correct_row, map_cache):
+    correct_row["Assertion"] = "Uncertain Significance"
+    return mock_koza(INGEST_NAME, correct_row, TRANSFORM_SCRIPT, map_cache=map_cache)
+
+
+# Test 'clinical_significance' values of 'Uncertain Significance'
+def test_correct_row_uncertain_significance(correct_entities_uncertain_significance):
+    assert len(correct_entities_uncertain_significance) == 3
+    entity, association_a, association_b = correct_entities_uncertain_significance
+    assert association_a.predicate == 'biolink:associated_with'
+    assert association_a.negated is False
+    assert association_a.original_predicate == 'Uncertain Significance'
+
+
+def test_invalid_clinical_significance(mock_koza, correct_row, map_cache):
+    correct_row["Assertion"] = "Invalid"
+    with pytest.raises(ValueError) as e_info:
+        mock_koza(INGEST_NAME, correct_row, TRANSFORM_SCRIPT, map_cache=map_cache)
+        assert str(e_info.value) == "Not sure how to handle _assertion: 'Invalid'"
